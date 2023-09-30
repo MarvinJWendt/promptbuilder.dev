9516c4afd2bfad9ae23ba0bba15fc5ea782266f8
MarvinJWendt <git@marvinjwendt.com>
2023-09-30 23:00:42 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,40 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig} from 'vite';
+import {SvelteKitPWA} from '@vite-pwa/sveltekit'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    SvelteKitPWA(
+      {
+        strategies: "generateSW",
+        devOptions: {
+          enabled: true,
+        },
+        manifest: {
+          name: "Prompt Builder",
+          short_name: "Prompt Builder",
+          description: "Prompt Builder is a free library and builder for ChatGPT prompts.",
+          lang: "en",
+          display: "standalone",
+          start_url: "/",
+          scope: "/",
+          theme_color: "#0e1728",
+          background_color: "#0e1728",
+          icons: [
+            {
+              src: "/android-chrome-192x192.png",
+              sizes: "192x192",
+              type: "image/png"
+            },
+            {
+              src: "/android-chrome-512x512.png",
+              sizes: "512x512",
+              type: "image/png"
+            },
+          ]
+        },
+      }
+    )
+  ]
 });
