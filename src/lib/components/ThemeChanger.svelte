d6309dd6d2fa24d770fae69457bbaac0417a23d5
MarvinJWendt <git@marvinjwendt.com>
2023-10-01 02:05:12 +0200

    update

--- src/lib/components/ThemeChanger.svelte
+++ src/lib/components/ThemeChanger.svelte
@@ -5,6 +5,17 @@
   onMount(() => {
     themeChange(false)
 
+    // if the html tag does not have the data-theme attribute, set it to the preferred theme (set by browser)
+    if (!document.documentElement.hasAttribute('data-theme')) {
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-theme', 'dark')
+        localStorage.setItem('theme', 'dark')
+      } else {
+        document.documentElement.setAttribute('data-theme', 'light')
+        localStorage.setItem('theme', 'light')
+      }
+    }
+
     const checkbox = document.getElementById('theme-selector-ckb')
     if (localStorage.getItem('theme') === 'light') {
       checkbox.checked = true
